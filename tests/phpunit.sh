80fc0c6abc89f54b23bd627b097a397a2ed26b3a
Jane Alesi <ja@satware.ai>
2025-11-15 07:56:50 +0100

    test: update PHPUnit configs and scripts for consistent error handling
    - Set `stopOnError` and `stopOnFailure` to `false` in phpunit.xml.
    - Removed `-it` flag from `docker exec` commands in test scripts for simpler Docker interaction.
    - Adjusted `phpunit.sh` to run Firebird 3 tests only, ensuring streamlined execution.

--- tests/phpunit.sh
+++ tests/phpunit.sh
@@ -2,7 +2,7 @@
 set -e  # Stop script on any command failure
 docker compose down --remove-orphans && docker compose up -d
 sleep 5
-echo Running Firebird 2.5 Testsuite:
-docker exec --user=application -it -w /app/tests app-doctrine-firebird-driver php -d memory_limit=4G ../vendor/bin/phpunit -c phpunit-firebird25.xml
+echo Running Firebird 3 Testsuite:
+docker exec --user=application -w /app/tests app-doctrine-firebird-driver php -d memory_limit=4G ../vendor/bin/phpunit -c phpunit.xml
 docker compose down
 echo Everything works as expected
