18862d9a8c9c99a388e900f3abfdf556936ee217
Michael Wegener <mw@satware.com>
2025-11-28 10:54:03 +0100

    Revamp testing setup with entrypoint script, improved Docker configurations, and test isolation
    - Added a standardized `entrypoint.sh` for better container initialization and command execution.
    - Updated `docker-compose.yml` to use dynamic UID/GID for improved compatibility.
    - Switched to `docker compose run` for PHPUnit and code quality checks, improving test execution reliability.
    - Adjusted Dockerfile to use a multi-stage build for cleaner image creation and added Composer installation.
    - Increased test startup wait time to ensure Firebird readiness.
    - Updated PHPUnit configuration for database connection string compatibility.

--- tests/docker-cqc.sh
+++ tests/docker-cqc.sh
@@ -6,6 +6,7 @@ cd "$SCRIPT_DIR"
 
 docker compose down --remove-orphans && docker compose up -d
 echo Running Code Quality Checks
-docker exec --user=application  app-doctrine-firebird-driver tests/cqc.sh
+docker compose run app composer install
+docker compose run app tests/cqc.sh
 docker compose down
 echo Excelent Code Quality
--- tests/phpunit.sh
+++ tests/phpunit.sh
@@ -5,8 +5,8 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 cd "$SCRIPT_DIR"
 
 docker compose down --remove-orphans && docker compose up -d
-sleep 1
+sleep 10
 echo Running Firebird 3 Testsuite:
-docker exec --user=application -w /app/tests app-doctrine-firebird-driver php ../vendor/bin/phpunit -c phpunit.xml "$@"
+docker compose run --rm app php vendor/bin/phpunit -c tests/phpunit.xml "$@"
 docker compose down
 echo All tests executed
